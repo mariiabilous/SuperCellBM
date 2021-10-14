c97633f1154e2bd573558296f61ec04abe91140a
mariiabilous <mariia.bilous@unil.ch>
2021-10-14 12:56:17 +0200

    plotting consitency of DEA

--- R/compute_supercells_DEA.R
+++ R/compute_supercells_DEA.R
@@ -104,6 +104,99 @@ compute_supercells_DEA <- function(
 }
 
 
+#' Computes DEA for single-cell data and for GT cell type annotation
+#' @param sc.ge single-cell gene expression
+#' @param clusters single-cell clustering result or GT cell type annotation
+#' @param ... rest of the parameters of \link{compute_supercells_DEA}
+#' @export
+compute_singglecell_DEA <- function(
+  sc.ge,
+  clusters,
+  ident.1 = NULL,
+  ident.2 = NULL,
+  seed = 12345,
+  pval.thresh = 0.05,
+  ...
+){
+
+  if(is.null(ident.1)){ # find all genes
+    cur.res <- SuperCell::supercell_FindAllMarkers(
+      ge = sc.ge,
+      clusters = clusters,
+      ...
+    )
+  } else {
+    idnt.name <- paste(ident.1, collapse = "_")
+    cur.res <- list()
+
+    cur.res[[idnt.name]] <- SuperCell::supercell_FindMarkers(
+      ge = sc.ge,
+      clusters = clusters,
+      ident.1 = ident.1,
+      ident.2 = ident.2,
+      ...
+    )
+  }
+
+  cur.res <- lapply(
+    cur.res,
+    FUN = function(x){
+      r <- x
+      mlt <- ifelse(as.numeric(r$adj.p.value) < pval.thresh, 1, 0)
+
+      print(sum(mlt)/length(mlt))
+
+      set.seed(seed)
+      r$logFC_AUC <- mlt * r$logFC +
+        (1 - mlt)*runif(min = 0, max = 1e-1, n = length(mlt)) * sign(r$logFC)
+
+      r$gamma.actual <- 1
+
+      return(r)
+    })
+
+  return(cur.res)
+}
+
+
+#' Compute bool vector of cluster-specific DEG
+#'
+#' @param DEA.GT  GT differentially expressed genes (DEA for GT annotation)
+#' @param logFC.thresh thereshold to consider DEG to be statistically significant
+#' @param pval.thresh thereshold to consider DEG to be statistically significant (keep the same as in \link{compute_supercells_DEA} and \link{compute_singlecells_DEA})
+#'
+#' @export
+
+compute_DEA_GT_bool <- function(
+  DEA.GT,
+  all.genes  = NULL,
+  logFC.thresh = 1,
+  pval.thresh = 0.05
+){
+  if(is.null(all.genes)){
+    all.genes <- sort(unique(unlist(lapply(DEA.GT, rownames))))
+    warning(paste("The entire set of genes was not provided, will use N =", length(all.genes), "genes to compute consistency of DEA"))
+  }
+
+  cell.types <- names(DEA.GT)
+
+  DEA.GT.signif <- lapply(DEA.GT, function(x){
+    rownames(x)[(x$adj.p.value < pval.thresh) & (x$logFC > logFC.thresh)]
+  })
+
+  signif.gene.cl <- c()
+  for(cl in names(DEA.GT.signif)){
+    signif.gene.cl <- c(signif.gene.cl, paste0(cl, "_", DEA.GT.signif[[cl]]))
+  }
+
+  res <- rep(FALSE, length(cell.types)*length(all.genes))
+  names(res) <- paste(rep(cell.types, each = length(all.genes)), rep(all.genes, length(cell.types)),  sep = "_")
+
+  res[signif.gene.cl] <- TRUE
+
+  return(res)
+}
+
 
 #' Compute consistency of super-cell (meta-cell) DEA with the GT DEA (DEA for GT annotation)
 #'
@@ -124,7 +217,8 @@ compute_consistency_of_supercell_DEA <- function(
 
   tpr.dea <- data.frame()
 
-  N.clusters    <- length(names(GT.DEA)) # GT number of clusters (cell types)
+  N.clusters    <- length(names(DEA.list)) # GT number of clusters (cell types)
+  N.markers     <- sum(GT.DEA.bool)
 
   for(meth in names(DEA.list)){
 
@@ -134,90 +228,118 @@ compute_consistency_of_supercell_DEA <- function(
 
         cur.DEA           <- DEA.list[[meth]][[gamma.ch]][[seed.i.ch]]
 
+        markers.logFC_AUC <- c()
+        ## TO DO: ADD cell line_gene column
         for(cl in names(cur.DEA)){
           cur.DEA.cl      <- cur.DEA[[cl]]
-          cur.GT.DEA.cl   <- GT.DEA[[cl]]
-          cur.N.genes     <- length(cur.GT.DEA.cl)
 
-          cur.markers.logFC_AUC     <- cur.DEA.cl$logFC_AUC
-          cur.markers.logFC_AUC     <- cur.markers.logFC_AUC[names(GT.DEA.bool)]
+          cur.markers.logFC_AUC         <- cur.DEA.cl$logFC_AUC
+          names(cur.markers.logFC_AUC)  <- paste(cl, rownames(cur.DEA.cl), sep = "_")
+          markers.logFC_AUC             <- c(markers.logFC_AUC, cur.markers.logFC_AUC)
 
-          cur.markers.logFC_AUC[is.na(cur.markers.logFC_AUC)] <- -1000
-          names(cur.markers.logFC_AUC)    <- names(GT.DEA.bool)
-
-          cur.rocit <- prediction(as.numeric(cur.markers.logFC_AUC), as.numeric(GT.DEA.bool))
-          cur.perf  <- performance(cur.rocit,"tpr","fpr")
-
-          cur.TPR   <- cur.perf@y.values[[1]]
-          cur.FPR   <- cur.perf@x.values[[1]]
-
-          cur.AUC   <- performance(cur.rocit,"auc")
-          cur.AUC   <- cur.AUC@y.values[[1]]
-
-          cur.F1    <- performance(cur.rocit,"f")
-          cur.F1    <- cur.F1@y.values[[1]]
-
-          cur.n.pos <- cur.rocit@n.pos.pred[[1]]
-
-          cur.df <- data.frame(
-            TPR = cur.TPR,
-            FPR = cur.FPR,
-            AUC = cur.AUC,
-            F1  = cur.F1,
-            N.pos = cur.n.pos,
-            Gamma = as.numeric(gamma.ch),
-            Gamma_actual = unique(cur.DEA.cl$gamma.actual),
-            Seed = as.numeric(seed.i.ch),
-            Method = meth
-          )
-
-          tpr.dea <- rbind(tpr.dea, cur.df)
         }
+
+        if(!is.null(markers.logFC_AUC)){
+          markers.logFC_AUC     <- markers.logFC_AUC[names(GT.DEA.bool)]
+        } else {
+          markers.logFC_AUC     <- rep(NA, length(GT.DEA.bool))
+        }
+
+        markers.logFC_AUC[is.na(markers.logFC_AUC)] <- -1000
+        names(markers.logFC_AUC)    <- names(GT.DEA.bool)
+
+
+        cur.rocit <- ROCR::prediction(as.numeric(markers.logFC_AUC), as.numeric(GT.DEA.bool))
+        cur.perf  <- ROCR::performance(cur.rocit,"tpr","fpr")
+
+        cur.TPR   <- cur.perf@y.values[[1]]
+        cur.FPR   <- cur.perf@x.values[[1]]
+
+        cur.AUC   <- ROCR::performance(cur.rocit,"auc")
+        cur.AUC   <- cur.AUC@y.values[[1]]
+
+        cur.F1    <- ROCR::performance(cur.rocit,"f")
+        cur.F1    <- cur.F1@y.values[[1]]
+
+        cur.n.pos <- cur.rocit@n.pos.pred[[1]]
+
+        cur.df <- data.frame(
+          TPR = cur.TPR,
+          FPR = cur.FPR,
+          AUC = cur.AUC,
+          F1  = cur.F1,
+          N.pos = cur.n.pos,
+          Gamma = as.numeric(gamma.ch),
+          Gamma_actual = unique(cur.DEA.cl$gamma.actual),
+          Seed = as.numeric(seed.i.ch),
+          Method = meth
+        )
+
+        cur.df <- cur.df[-nrow(cur.df),]
+
+        cur.df <- cur.df %>%
+          filter(N.pos == min(N.markers, max(N.pos)))
+
+        tpr.dea <- rbind(tpr.dea, cur.df)
+
       }
     }
   }
 
   #####  for Gamma == 1
-  for(cl in names(sc.DEA)){
+  markers.logFC_AUC <- c()
+  for(cl in names(cur.DEA)){
     cur.DEA.cl      <- sc.DEA[[cl]]
-    cur.GT.DEA.cl   <- GT.DEA[[cl]]
-    cur.N.genes     <- length(cur.GT.DEA.cl)
 
-    cur.markers.logFC_AUC     <- cur.DEA.cl$logFC_AUC
-    cur.markers.logFC_AUC     <- cur.markers.logFC_AUC[names(GT.DEA.bool)]
+    cur.markers.logFC_AUC         <- cur.DEA.cl$logFC_AUC
+    names(cur.markers.logFC_AUC)  <- paste(cl, rownames(cur.DEA.cl), sep = "_")
+    markers.logFC_AUC             <- c(markers.logFC_AUC, cur.markers.logFC_AUC)
 
-    cur.markers.logFC_AUC[is.na(cur.markers.logFC_AUC)] <- -1000
-    names(cur.markers.logFC_AUC)    <- names(GT.DEA.bool)
+  }
 
-    cur.rocit <- prediction(as.numeric(cur.markers.logFC_AUC), as.numeric(GT.DEA.bool))
-    cur.perf  <- performance(cur.rocit,"tpr","fpr")
+  if(!is.null(markers.logFC_AUC)){
+    markers.logFC_AUC     <- markers.logFC_AUC[names(GT.DEA.bool)]
+  } else {
+    markers.logFC_AUC     <- rep(NA, length(GT.DEA.bool))
+  }
 
-    cur.TPR   <- cur.perf@y.values[[1]]
-    cur.FPR   <- cur.perf@x.values[[1]]
+  markers.logFC_AUC[is.na(markers.logFC_AUC)] <- -1000
+  names(markers.logFC_AUC)    <- names(GT.DEA.bool)
 
-    cur.AUC   <- performance(cur.rocit,"auc")
-    cur.AUC   <- cur.AUC@y.values[[1]]
 
-    cur.F1    <- performance(cur.rocit,"f")
-    cur.F1    <- cur.F1@y.values[[1]]
+  cur.rocit <- ROCR::prediction(as.numeric(markers.logFC_AUC), as.numeric(GT.DEA.bool))
+  cur.perf  <- ROCR::performance(cur.rocit,"tpr","fpr")
 
-    cur.n.pos <- cur.rocit@n.pos.pred[[1]]
+  cur.TPR   <- cur.perf@y.values[[1]]
+  cur.FPR   <- cur.perf@x.values[[1]]
 
-    for(meth in names(DEA.list)){
-      cur.df <- data.frame(
-        TPR = cur.TPR,
-        FPR = cur.FPR,
-        AUC = cur.AUC,
-        F1  = cur.F1,
-        N.pos = cur.n.pos,
-        Gamma = 1,
-        Gamma_actual = 1,
-        Seed = as.numeric(names(DEA.list[[meth]][[gamma.ch]]))[1],
-        Method = meth
-      )
+  cur.AUC   <- ROCR::performance(cur.rocit,"auc")
+  cur.AUC   <- cur.AUC@y.values[[1]]
 
-      tpr.dea <- rbind(tpr.dea, cur.df)
-    }
+  cur.F1    <- ROCR::performance(cur.rocit,"f")
+  cur.F1    <- cur.F1@y.values[[1]]
+
+  cur.n.pos <- cur.rocit@n.pos.pred[[1]]
+
+  for(meth in names(DEA.list)){
+    cur.df <- data.frame(
+      TPR = cur.TPR,
+      FPR = cur.FPR,
+      AUC = cur.AUC,
+      F1  = cur.F1,
+      N.pos = cur.n.pos,
+      Gamma = 1,
+      Gamma_actual = 1,
+      Seed = as.numeric(names(DEA.list[[meth]][[gamma.ch]]))[1],
+      Method = meth
+    )
+
+    cur.df <- cur.df[-nrow(cur.df),]
+
+    cur.df <- cur.df %>%
+      filter(N.pos == min(N.markers, max(N.pos)))
+
+    tpr.dea <- rbind(tpr.dea, cur.df)
   }
 
   return(tpr.dea)
@@ -238,56 +360,47 @@ compute_consistency_of_supercell_DEA <- function(
 #'
 
 plot_DEA_consistency <- function(
-  clust.consistency.df,
+  DEA.consistency.df,
   consistency.index.name = 'TPR',
-  min.value.alt.clustering = 0,
   error_bars = c('extr', 'quartiles', 'sd')[1],
   SC_meth_exclude = c(),
   to.save.plot = TRUE,
   to.save.plot.raw = FALSE,
   asp = 0.5,
   fig.folder = './plots',
-  .shapes = c("Exact"=1, "Approximate"=0, "Subsampling"=2, "Random"=3,
+  .shapes = c("Exact"=1, "Approx"=0, "Subsampling"=2, "Random"=3,
               "Metacell_default_fp"=4, "Metacell_default_av" = 8,
-              "Metacell_SC_like_fp"=4, "Metacell_SC_like_av" = 8, "Alternative" = 23),
-  .colors = c("Exact"="darkred", "Approximate"="royalblue", "Subsampling"="black", "Random"="gray",
+              "Metacell_SC_like_fp"=4, "Metacell_SC_like_av" = 8),
+  .colors = c("Exact"="darkred", "Approx"="royalblue", "Subsampling"="black", "Random"="gray",
               "Metacell_default_fp"="forestgreen", "Metacell_default_av" = "forestgreen",
-              "Metacell_SC_like_fp"="gold", "Metacell_SC_like_av" = "gold", "Alternative" = "darkblue"),
+              "Metacell_SC_like_fp"="gold", "Metacell_SC_like_av" = "gold"),
   verbose = FALSE,
   ...
 ){
 
   `%>%` <- dplyr::`%>%`
 
-  if(consistency.index.name %in% colnames(clust.consistency.df)){
-    clust.consistency.df[["Score"]] <- clust.consistency.df[[consistency.index.name]]
+  if(consistency.index.name %in% colnames(DEA.consistency.df)){
+    DEA.consistency.df[["Score"]] <- DEA.consistency.df[[consistency.index.name]]
   } else {
     stop(paste("consistency.index.name:", consistency.index.name, "not found in clust.consistency.df,
-               available values are:", paste(colnames(clust.consistency.df), collapse = ', ')))
+               available values are:", paste(colnames(DEA.consistency.df), collapse = ', ')))
   }
 
-  clust_name <- clust.consistency.df[['Clustering_name']][1]
-
-  ## Fiter non-realistic alternative clustering results
-  clust.consistency.df <- clust.consistency.df %>%
-    dplyr::filter(
-      Score >= min.value.alt.clustering | Method != 'Alternative')
-
-  ## Compute summary
-  clust.consistency.df_summarized <- clust.consistency.df %>%
-    dplyr::group_by(Method, Gamma) %>%
-    dplyr::summarize(
+  DEA.consistency.df_summarized <- DEA.consistency.df %>%
+    group_by(Method, Gamma_actual) %>%
+    summarise(
       meanScore   = mean(Score),
       firstQScore = unname(summary(Score)[2]),
       thirdQScore = unname(summary(Score)[5]),
       medianScore = median(Score),
-      sdScore     = sd(Score),
+      sdScore     = ifelse(!is.na(sd(Score)), sd(Score), 0),
       minScore     = min(Score),
       maxScore     = max(Score),
-      medianPsd    = min(median(Score)+sd(Score), 1),
-      medianMsd    = median(Score)-sd(Score))
+      medianPsd    = min(median(Score)+ifelse(!is.na(sd(Score)), sd(Score), 0), 1),
+      medianMsd    = max(median(Score)-ifelse(!is.na(sd(Score)), sd(Score), 0), 0)
+    )
 
-  clust.consistency.df_summarized[is.na(clust.consistency.df_summarized)] <- 0
 
   if(is.null(error_bars)){
     error_bars <- 'extr'
@@ -322,7 +435,7 @@ plot_DEA_consistency <- function(
 
 
   ## Plot across gamma
-  df.to.plot <- clust.consistency.df_summarized %>%
+  df.to.plot <- DEA.consistency.df_summarized %>%
     dplyr::filter(
       !(Method %in% SC_meth_exclude))
 
@@ -330,7 +443,7 @@ plot_DEA_consistency <- function(
   df.to.plot[['max_err_bar']] <- df.to.plot[[max_err_name]]
 
 
-  g <- ggplot2::ggplot(df.to.plot, ggplot2::aes(x = Gamma, y = medianScore, color = Method, fill = Method,  shape = Method)) +
+  g <- ggplot2::ggplot(df.to.plot, ggplot2::aes(x = Gamma_actual, y = medianScore, color = Method, fill = Method,  shape = Method)) +
     ggplot2::geom_point() +
     ggplot2::geom_line() +
     ggplot2::geom_errorbar(
@@ -340,7 +453,7 @@ plot_DEA_consistency <- function(
     ggplot2::scale_fill_manual(values = .colors) +
     ggplot2::scale_shape_manual(values = .shapes) +
     ggplot2::scale_x_log10() +
-    ggplot2::labs(x = 'Graining level', y = paste0(consistency.index.name, ' (', clust_name,')'))
+    ggplot2::labs(x = 'Graining level', y = paste0(consistency.index.name))
 
   plot(g)
 
@@ -349,7 +462,7 @@ plot_DEA_consistency <- function(
     if(!dir.exists(fig.folder.save))
       dir.create(fig.folder.save, recursive = TRUE)
 
-    filename = paste0(consistency.index.name, '_clustering_', clust_name, '_errbar_', error_bars)
+    filename = paste0(consistency.index.name, '_errbar_', error_bars)
     SCBM_saveplot(p = g, folder = fig.folder.save, name = filename, save.raw.ggplot = FALSE, asp = asp, ...)
   }
   return(df.to.plot)
@@ -357,59 +470,3 @@ plot_DEA_consistency <- function(
 }
 
 
-#' Compute alternative clustering at single-cell level
-#'
-#' @param sc.pca PCA matrix or high dimensional matrix (cells as rows and coordinates/genes as columns)
-#' @param N.comp number or vector of PCs to use
-#' @param N.clusters.seq vector of number of clusters to compute
-#' @param hclust_methods a vector of \code{method} parameter form \link[stats]{hclust}
-#' @param other_clust_funcs list of ther clustering functions (in a format of \link[stats]{kmeans})
-#'
-#' @return list of clusterings \code{res} in a format \code{res[["number_of_clusters"]][["clustering_method[_random_seed]"]]}
-#'
-#' @export
-#'
-compute_alternative_clustering <- function(
-  sc.pca,
-  N.comp = 10,
-  N.clusters.seq = c(2:10),
-  hclust_methods = c("ward.D", "ward.D2", "single", "complete", "average", "mcquitty", "median", "centroid"),
-  other_clust_funcs = list("kmeans" = kmeans),
-  seed.seq = c(12346, 111, 19, 42, 7)
-){
-  if(length(N.comp) == 1)
-    N.comp = 1:N.comp
-  print(N.comp)
-
-  d <- dist(sc.pca[,N.comp])
-
-  sc.clust <- list()
-  for(k.ch in as.character(N.clusters.seq)){
-    sc.clust[[k.ch]] <- list()
-  }
-
-  # different hierarchical clusterings
-  for(hclust_meth in hclust_methods){
-    cur.clust <- hclust(d = d, method = hclust_meth)
-    for(k in N.clusters.seq){
-      k.ch <- as.character(k)
-      sc.clust[[k.ch]][[hclust_meth]] <- cutree(cur.clust, k = k)
-    }
-  }
-
-  # k-means with different random seeds
-  for(clust_name in names(other_clust_funcs)){
-    f <- other_clust_funcs[[clust_name]]
-    for(k in N.clusters.seq){
-      k.ch <- as.character(k)
-      for(seed.i in seed.seq){
-        cur.clustering.name <- paste(clust_name, seed.i, sep = "_")
-        set.seed(seed.i)
-
-        cur.clust <- kmeans(sc.pca[,N.comp], k)$cluster
-        sc.clust[[k.ch]][[cur.clustering.name]] <- cur.clust
-      }
-    }
-  }
-  return(sc.clust)
-}
